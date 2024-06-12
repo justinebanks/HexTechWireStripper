49731c335d6b3b5769f766fa36c596dd7b983dfb
justinebanks <justinbanks580@gmail.com>
2024-06-12 16:51:40 -0400

    Finishes the First Draft of the evaluateCommands() function

--- main.js
+++ main.js
@@ -21,33 +21,67 @@ const LIN_MOT_STEP_DIST = 0;
 const EXTRUDER_STEP_DIST = 0
 
 let recordedCurrentPosition = 0;
+let commandArray = [];
 
 
 function moveTopBlade(distance) {
     dist = LIN_MOT_STEP_DIST * distance;
-    return `${LIN_MOT_ONE}_move_${distance};${LIN_MOT_TWO}_move_${dist}`;
+    command = `${LIN_MOT_ONE}_move_${distance};${LIN_MOT_TWO}_move_${dist}`
+    commandArray.push(command);
+    return command;
 }
+
 
 function moveWire(distance) {
     dist = EXTRUDER_STEP_DIST * distance;
-    return `${EXTRUDER_STEPPER}_move_${dist}`;
+    command = `${EXTRUDER_STEPPER}_move_${dist}`;
+    commandArray.push(command);
+    return command;
 }
 
 
+// Evaluates commandArray Based On Input Values
 function evaluateCommand() {
-    moveWire(leftStripLength.value);
-    moveTopBlade((WIRE_Y_POSITION - recordedCurrentPosition) + stripDepth);
-    moveTopBlade(1000);
-    
+    commandArray.clear()
+
+    for (let i = 0; i < wireQuantity.value; i++) {
+        // Left Strip
+        moveWire(leftStripLength.value);
+        moveTopBlade((WIRE_Y_POSITION - recordedCurrentPosition) + stripDepth.value); // To Stripping Depth
+
+        // Cut
+        moveTopBlade(1000);
+        moveWire(wireLength.value);
+        moveTopBlade((WIRE_Y_POSITION - recordedCurrentPosition) + stripDepth.value); // To Stripping Depth
+
+        // Right Strip
+        moveTopBlade(1000);
+        moveWire(rightStripLength.value);
+        moveTopBlade((WIRE_Y_POSITION - recordedCurrentPosition)); // To Cutting Depth
+    }
+}
+
+
+// Turns commandArray Into A Single Command String
+function commandArrayToString() {
+    let returnString = "";
+
+    for (let i = 0; i < commandArray.length; i++) {
+        returnString = returnString + "|" + commandArray[i]
+    }
+
+    return returnString;
 }
 
 
 function onSubmit() {
     console.log("Submit Button Clicked");
-    command = evaluateCommand();
+
+    evaluateCommand();
+    command = commandArrayToString();
 
     client.publish(
         "hextech/hextech-justin/commands",
-        "led.bl_off"
+        "led.bl_off" //command
     );
 }
