8df41b7a71dbbdea1d9fe052f179bd176281aa93
justinebanks <justinbanks580@gmail.com>
2024-06-17 15:18:11 -0400

    Work from 6.17.24

--- main.js
+++ main.js
@@ -19,9 +19,48 @@ const EXTRUDER_STEPPER = "stepper.02"; // rywb4-rwyb6
 const WIRE_Y_POSITION = 0;
 const LIN_MOT_STEP_DIST = 1;
 const EXTRUDER_STEP_DIST = 1;
+const BOTTOM_BLADE_POSITION = -7000;
 
-let recordedCurrentPosition = 0;
 let commandArray = [];
+let bladeHistory = [];
+
+
+function emergencyStop() {
+    runSerialCommand("P_stepper.00_stop;P_stepper.01_stop;P_stepper.02_stop");
+}
+
+function initializeCurrent() {
+    runSerialCommand("stepper.00_rms_600;stepper.01_rms_600;stepper.02_rms_300");
+}
+
+
+function getBladeRelativePosition() {
+    let distFromHome = 0;
+
+    for (let i = 0; i < bladeHistory.length; i++) {
+        distFromHome += bladeHistory[i];
+    }
+
+    return distFromHome;
+}
+
+
+function goHome() {
+    runSerialCommand(moveTopBlade(-getBladeRelativePosition()));
+    bladeHistory = [];
+}
+
+
+// Turns commandArray Into A Single Command String
+function commandArrayToString() {
+    let returnString = "";
+
+    for (let i = 0; i < commandArray.length; i++) {
+        returnString = returnString + "|" + commandArray[i];
+    }
+
+    return returnString;
+}
 
 
 function runSerialCommand(command) {
@@ -38,59 +77,69 @@ function runSerialCommand(command) {
 
 function moveTopBlade(distance) { // Positive for Up, Negative for Down
     dist = LIN_MOT_STEP_DIST * distance;
-    command = `${LIN_MOT_ONE}_move_${distance*-1};${LIN_MOT_TWO}_move_${dist}`
+    command = `${LIN_MOT_ONE}_move_${distance*-1};${LIN_MOT_TWO}_move_${dist}`;
+
     commandArray.push(command);
+    bladeHistory.push(distance);
+
     return command;
 }
 
 
-function moveWire(distance) { // Positive for Backward, Negative for Forward
-    dist = EXTRUDER_STEP_DIST * distance;
+function moveWire(distance) { // Positive for Forward, Negative for Backward
+    dist = EXTRUDER_STEP_DIST * distance
     command = `${EXTRUDER_STEPPER}_move_${dist}`;
     commandArray.push(command);
     return command;
 }
 
 
-// Evaluates commandArray Based On Input Values
-function evaluateCommand() {
-    commandArray.clear()
-
-    for (let i = 0; i < wireQuantity.value; i++) {
-        // Left Strip
-        moveWire(-leftStripLength.value);
-        moveTopBlade((WIRE_Y_POSITION - recordedCurrentPosition) + stripDepth.value); // To Stripping Depth
-
-        // Cut
-        moveTopBlade(1000);
-        moveWire(wireLength.value);
-        moveTopBlade((WIRE_Y_POSITION - recordedCurrentPosition) + stripDepth.value); // To Stripping Depth
-
-        // Right Strip
-        moveTopBlade(1000);
-        moveWire(rightStripLength.value);
-        moveTopBlade((WIRE_Y_POSITION - recordedCurrentPosition)); // To Cutting Depth
-    }
+function cutWire() {
+    return moveTopBlade(BOTTOM_BLADE_POSITION - getBladeRelativePosition())
 }
 
 
-// Turns commandArray Into A Single Command String
-function commandArrayToString() {
-    let returnString = "";
+function stripWire() {
+    return moveTopBlade(BOTTOM_BLADE_POSITION - getBladeRelativePosition() + parseInt(stripDepth.value))
+}
 
-    for (let i = 0; i < commandArray.length; i++) {
-        returnString = returnString + "|" + commandArray[i]
-    }
 
-    return returnString;
+function testSequence() {
+    commandArray = []
+    moveWire(2000);
+    moveTopBlade(-5000);
+    moveWire(-1500);
+    moveTopBlade(5000);
+    runSerialCommand(commandArray);
+}
+
+
+function runSequence() {
+    commandArray = [];
+
+    moveWire(1000);
+    stripWire();
+    moveTopBlade(1000);
+
+    runSerialCommand(commandArray);
 }
 
 
 function onSubmit() {
     console.log("Submit Button Clicked");
+    runSequence();
 
-    evaluateCommand();
-    command = commandArrayToString();
-
-    runSerialCommand(command);
+    console.log("Finished");
 }
+
+// Brings Top Blade to "home" position when page is closed or refreshed
+window.onbeforeunload = () => {
+    //goHome();
+}
+
+// Should Start Printing Response Messages from MQTT Server
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log("MQTT Response: " + message.toString())
+    client.end()
+})
